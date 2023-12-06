316d0b5b047f2bc506ab2e2dd5e9ce3831816040
Nick <nickq4504@gmail.com>
2023-12-06 15:41:30 -0330

    -Form route created in app.py for receiving input. -Submit button added in index.html.

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
 from flask import Flask, render_template, request
 from wtforms import StringField, SubmitField
+from joblib import dump, load
+from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='my_secret_key'
 
 class MyForm(FlaskForm):
     name = StringField("Name")
@@ -12,6 +15,14 @@ class MyForm(FlaskForm):
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/form")
+@app.route("/form", methods=['GET', 'POST'])
 def formPage():
-    return render_template("index.html")
+    nameForm = MyForm()
+
+    if(request.method == "POST" and nameForm.is_submitted()):
+        nameInputData = nameForm.name.data
+        print("submitted")
+        print(nameInputData)
+
+
+    return render_template("index.html", form=nameForm)
